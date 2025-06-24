17bc6f838bb8897ac0ed7798b8de140de34f03c0
Adit Karode <karode.ad@northeastern.edu>
2025-06-24 16:29:21 -0700

    Fresh start

--- frontend/src/components/ui/use-toast.ts
+++
@@ -1,8 +0,0 @@
-// Copied from shadcn/ui toast component
-// https://ui.shadcn.com/docs/components/toast
-
-import { useToast as useToastOriginal, Toast } from "@/components/ui/toast"
-
-export const useToast = useToastOriginal
-
-export type { Toast } 
